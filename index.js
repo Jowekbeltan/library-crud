5ea9a42ab524f44467c694b36db7e0143bbd3eea
Jowekbeltan <ontwekajohnson502@gmail.com>
2025-10-01 14:54:01 +0300

    commit

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-// Root route - Add this before your other routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+// Root route
 app.get('/', (req, res) => {
     res.send(`
         <h1>Library CRUD API</h1>
@@ -14,3 +20,17 @@ app.get('/', (req, res) => {
         <p>Visit <a href="/books">/books</a> to see all books</p>
     `);
 });
+
+// Your existing routes
+const usersRoutes = require('./routes/users');
+const booksRoutes = require('./routes/books');
+const loansRoutes = require('./routes/loans');
+const reservationsRoutes = require('./routes/reservations');
+
+app.use('/users', usersRoutes);
+app.use('/books', booksRoutes);
+app.use('/loans', loansRoutes);
+app.use('/reservations', reservationsRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
